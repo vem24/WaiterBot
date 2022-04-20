ce7ea06b3bcd460487c4b29a47152892613129b8
vika084 <59125867+vem24@users.noreply.github.com>
2022-04-19 20:51:44 -0700

    Refactoring Completed

--- waiter_bot.py
+++ waiter_bot.py
@@ -6,50 +6,46 @@ import time
 
 # Refactor repetative print and time.sleep() code
 def print_pause(string):
-    print(str(string))
+    print(string)
     time.sleep(2)
 
 def intro():
-# Bot will introduce themselves
-print_pause("Hello! I am Bob, the Breakfast Bot.")
-# Bot will type today's menu
-print_pause("Today we have two breakfasts available.")
-print_pause("The first is waffles with strawberries and whipped cream.")
-print_pause("The second is sweet potato pancakes with butter and syrup.")
+    print_pause("Hello! I am Bob, the Breakfast Bot.")
+    print_pause("Today we have two breakfasts available.")
+    print_pause("The first is waffles with strawberries and whipped cream.")
+    print_pause("The second is sweet potato pancakes with butter and syrup.")
 
 def valid_input(prompt, option1, option2):
     while True:
         response = input(prompt).lower()
         if option1 in response:
-            return response
+            break
         elif option2 in response:
-            return response
+            break
         else:
             print_pause("Sorry, I don't understand.")
+    return response
 
 def get_order():
-    response = valid_input("Please place your order. "
-                    "Would you like waffles or pancakes?\n", "waffles", "pancakes")
+    response = valid_input("Please place your order. Would you like waffles or pancakes?\n", "waffles", "pancakes")
     if "waffles" in response:
-        print_pause("Waffles it is!")
-        
+        print_pause("Waffles it is!")     
     elif "pancakes" in response:
         print_pause("Pancakes it is!")
-
-intro()
-
-while True:
-
-    get_order()
-    # Let cusomer know their order will be ready soon.            
     print_pause("Your order will be ready shortly.")
-   
-    # Ask customer if they'd like to place another order.
-    order_again = valid_input("Would you like to place another order? "
-                        "Please say 'yes' or 'no'.\n", "no", "yes")
-    if "no" in order_again:
+    order_again()
+
+def order_again():
+    response = valid_input("Would you like to place another order? Please say 'yes' or 'no'.\n", "no", "yes")
+
+    if "no" in response:
         print_pause("OK, goodbye!")
-        break
-    elif "yes" in order_again:
+    elif "yes" in response:
         print_pause("Very good, I'm happy to take another order.")
-    
+        get_order()
+
+def order_breakfast():
+    intro()
+    get_order()
+
+order_breakfast()
